125d8377c8af58f32751388c24b4d7963f6f44db
Sarthak Maheshwari <sarthakmaheshwaribti@gmail.com>
2024-07-24 12:00:43 +0530

    final commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+// app.get('/',(req,res)=> {
+//     res.send("Hello world!");
+// })
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
+
+server.listen(PORT,()=> {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
+})
